eff961aba43a870ccc17d8dd05bef6ed536920f5
Troy mangrum <tmmangrum@gmail.com>
2020-06-17 21:40:33 -0500

    Added registration form, working next on getting everything running

---
+++ co-make-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import RegistrationForm from './component/RegistrationForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RegistrationForm>
+        SignUp
+      </RegistrationForm>
+    </div>
+  );
+}
+
+export default App;
