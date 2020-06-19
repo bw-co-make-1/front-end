1f695626667226cc18eea617f6821bfcd8174de1
Troy mangrum <tmmangrum@gmail.com>
2020-06-18 21:44:11 -0500

    updated

--- co-make-app/src/App.js
+++ co-make-app/src/App.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import RegistrationForm from './component/RegistrationForm';
-import logo from './logo.svg';
+import SignInForm from './component/SignInForm';
+import {Route, Link} from 'react-router-dom';
+import { Button, Navbar } from 'reactstrap';
 import './App.css';
 
 function App() {
   return (
+    <>
+    
     <div className="App">
-      <RegistrationForm>
-        Sign Up
-      </RegistrationForm>
-     
+      <Navbar>
+        <h1> Co-Make </h1>
+          <Link to={'/'}>
+            <Button>
+              Home
+            </Button>
+          </Link>
+          <Link to={'/register'}>
+            <Button>
+              Register
+            </Button>
+          </Link>
+          <Link to={'/signIn'}>
+            <Button>
+             Sign In
+            </Button>
+          </Link>
+      </Navbar>
+      <Route exact path='/'>Home</Route>
+      <Route path='/register'>
+        <RegistrationForm/>
+      </Route>
+      <Route path='/signIn'>
+        <SignInForm/>
+      </Route>
+      
+      
     </div>
+    </>
   );
-}
+};
 
 export default App;
